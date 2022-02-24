86cda10a470949d81e2b053dc59e3a84ee918332
Aibek <ai.bek@mail.ru>
2022-02-24 13:22:15 +0600

    task half done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   console.log(str, bracketsConfig)
+   let open = [];
+   let pair = {};
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      open.push(bracketsConfig[i][0]);
+      pair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+   }
+   console.log(open, pair)
+   let stack = [];
+   for (let i = 0; i < str.length; i++) {
+      let current = str[i];
+      if (open.includes(current)) {
+         stack.push(current);
+      }
+      else {
+         if (stack.length === 0) {
+            return false
+         }
+
+         let topElm = stack[stack.length - 1];
+         if (pair[current] == topElm) {
+            stack.pop();
+         }
+         else {
+            return false
+         }
+
+
+      }
+   }
+   return stack.length === 0
 }
+
+//console.log(check('([{}])', config3));
